37369b2ceed69a94cdec08a8fb89e1da57a0b308
Andre Filho <andre.filho001@outlook.com>
2019-02-16 11:48:06 -0200

    fix(docker): make corrections to dev env

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Landing from '@/components/Landing'
+import PageComponent from '@/components/pages/PageView'
 
 Vue.use(Router)
 
@@ -8,8 +9,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Landing',
+      component: Landing
+    },
+
+    {
+      path: '/:name',
+      name: 'ShowPage',
+      component: PageComponent
     }
   ]
 })
