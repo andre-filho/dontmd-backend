3a946f44daca4f493514b0990f52b06fef73de62
Andre Filho <andre.filho001@outlook.com>
2019-02-18 20:46:33 -0300

    add(landing page): landing page and added axios

---
+++ frontend/src/components/pages/PageView.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <b-row>
+      <b-col>
+        <!-- editor -->
+      </b-col>
+      <b-col>
+        <!-- preview -->
+      </b-col>
+    </b-row>
+  </section>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
